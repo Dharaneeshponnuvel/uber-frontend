4e791977a8f7df0846047078d070271136743838
Dharaneesh p <146561378+Dharaneeshponnuvel@users.noreply.github.com>
2025-09-04 21:56:22 +0530

    Update RideHistory.tsx

--- src/pages/RideHistory.tsx
+++ src/pages/RideHistory.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Clock, MapPin, Star, Calendar, Filter, DollarSign } from 'lucide-react';
-
 import { useAuth } from '../contexts/AuthContext';
 import axios from 'axios';
 
